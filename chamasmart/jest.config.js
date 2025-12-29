556612ef57f2c9cc6b0f0171479cc388fc4cb95b
Lewis Abuga Bosire <bosayajemo@gmail.com>
2025-12-29 15:19:17 +0300

    "feat: implement comprehensive testing and code quality improvements - Set up Jest and React Testing Library - Add unit tests for utility functions - Create reusable Button component with tests - Set up test coverage reporting - Configure ESLint and Prettier - Add GitHub Actions CI workflow - Update documentation with testing instructions

---
+++ chamasmart/jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  testMatch: [
+    '**/__tests__/**/*.test.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/index.js',
+    '!src/serviceWorker.js',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+};
