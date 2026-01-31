746125bf061a0a270f27213bbebe515ff25b448a
Lewis Abuga Bosire <bosayajemo@gmail.com>
2026-01-31 13:39:19 +0300

    change

--- chamasmart/frontend/src/main.jsx
+++ chamasmart/frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { registerSW } from 'virtual:pwa-register';
+import { registerSW } from "virtual:pwa-register";
 import ErrorBoundary from "./components/ErrorBoundary";
 
 // Register PWA service worker
@@ -13,5 +13,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
