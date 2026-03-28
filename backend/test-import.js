fe9088db71ff1d3af62d73bb1952f5929a3cd1f0
Lewis Abuga Bosire <bosayajemo@gmail.com>
2026-03-28 15:37:53 +0300

    refactor: remove unused legacy configuration files

--- backend/test-import.js
+++
@@ -1,9 +0,0 @@
-try {
-    console.log('Attempting to require server.js...');
-    const app = require('./server');
-    console.log('Server.js required successfully!');
-} catch (err) {
-    console.error('FAILED to require server.js');
-    console.error(err);
-    process.exit(1);
-}
